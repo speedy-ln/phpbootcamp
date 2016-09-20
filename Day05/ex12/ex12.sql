1c1bf2cf4e01c4844897aad306a26cd3b4638935
speedy_ln <lnnkadimeng@gmail.com>
2016-09-20 17:07:15 +0200

    ex13

---
+++ Day05/ex12/ex12.sql
@@ -0,0 +1,5 @@
+SELECT user_card.last_name,
+	user_card.first_name
+FROM user_card
+WHERE user_card.last_name LIKE '%-%' OR user_card.first_name LIKE '%-%'
+ORDER BY user_card.last_name ASC, user_card.first_name ASC;
